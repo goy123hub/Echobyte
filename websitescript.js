7a3009fee386b2dab8b70fd49d5f8d4101d58412
goy123hub <163375862+goy123hub@users.noreply.github.com>
2024-03-20 10:56:05 +0700

    Add files via upload

---
+++ websitescript.js
@@ -0,0 +1,39 @@
+const openCME = () => {
+    window.location.href = 'https://docs.google.com/document/d/1q9RmCA7Qzt_exSRZz8-wVyM6m0VfvlDE0ew9oE-3oRs/edit'
+}
+const openHEC = () => {
+    window.location.href = 'https://docs.google.com/document/d/1nO432PZp2lrgzNpUOWewi4AEAnxYb4sdBDwiOgMsBQU/edit'
+}
+const openPhysics = () => {
+    window.location.href = 'https://docs.google.com/document/d/1QIeUxX4tpq3HWuI-5v6UvZY7nG4PAcWxyS0UxM0OF0o/edit'
+}
+const openChemistry = () => {
+    window.location.href = 'https://docs.google.com/document/d/1ttMt45BpIC0DKmouhJheSvfJRk1m-AL93pAME1uHUdI/edit'
+}
+const openPE = () => {
+    window.location.href = 'https://drive.google.com/file/d/1pN3PNfUnyNQGP5Jnff3No3SVWvpgTLAd/view?usp=sharing'
+}
+const openEco = () => {
+    window.location.href = 'https://drive.google.com/file/d/1lxa_E2RtlmUeqn1hjDeyUxrpcplumEN-/view?usp=drivesdk'
+}
+const openReli = () => {
+    window.location.href = 'https://drive.google.com/file/d/1nsVVM0BFlO5l0NgghxNDp2o-ZZguCQlS/view?usp=drivesdk'
+}
+const openMandarin = () => {
+    window.location.href = 'https://drive.google.com/file/d/1mhv-QeGpsrUbEfpNLQIwyxV33o5OOKp_/view?usp=sharing'
+}
+const openGerman = () => {
+    window.location.href = 'https://drive.google.com/drive/folders/1QWHJL96qz2MktlwfGPPQSnHm1HtQm0wL?usp=sharing'
+}
+const openArt = () => {
+    window.location.href = 'https://www.canva.com/design/DAF_602Ekfo/Pdy7U-ILkwU4VhK7ir0ARg/edit?utm_content=DAF_602Ekfo&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'
+}
+const openICT = () => {
+    window.location.href = 'https://drive.google.com/file/d/1MhP_vtILVsJcLwqJs8Opk1mszz9V1r6N/view?usp=sharing'
+}
+const openBI = () => {
+    window.location.href = 'https://docs.google.com/document/d/1JBtDW6uIphusXsRA8FzbFMPGgpX18afPe085AC0ErEc/edit'
+}
+const openMath = () => {
+    window.location.href = 'https://drive.google.com/file/d/1pHOTR08NWwfYZaOWYyCCvDL60NO-Snpb/view?usp=sharing'
+}
